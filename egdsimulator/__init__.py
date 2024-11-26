318511a9f8da5d47bf45e8afe79f97d9b881abec
Mauro Mugnaini <mauro.mugnaini@nubisware.com>
2024-11-26 10:53:34 +0100

    Reformatted to solve indentations problems in python3

--- egdsimulator/__init__.py
+++ egdsimulator/__init__.py
@@ -2,7 +2,6 @@
 
 import threading
 import time
-import datetime
 import socket
 import random
 import struct
@@ -10,81 +9,94 @@ import egdmodel
 
 class EGDSimulator():
 
-		def __init__(self, egd, address="127.0.0.255", fixed = False):
-			self.egd = egd
-			self.sock = None
-			self.socklock = threading.Lock()
-			self.count = 0
-			self.address = address
-			self.fixed = fixed
+    def __init__(self, egd, address="127.0.0.255", fixed=False):
+        self.egd = egd
+        self.sock = None
+        self.socklock = threading.Lock()
+        self.count = 0
+        self.address = address
+        self.fixed = fixed
 
-		def computeValue(self, tag):
-			if tag.type in ["REAL","LREAL"]:
-				tag.lastvalue = tag.lastvalue if tag.lastvalue != None else (-1000.0 + random.random() * 2000.0)
-				tag.lastvalue += 0 if self.fixed else -10 + random.random() * 20
-			elif tag.type == "DINT":
-				tag.lastvalue = tag.lastvalue if tag.lastvalue != None else random.randrange(-1000,1000)
-				tag.lastvalue += 0 if self.fixed else random.randrange(-10,11) 
-			elif tag.type == "INT":
-				tag.lastvalue = tag.lastvalue if tag.lastvalue != None else random.randrange(-100,100)
-				tag.lastvalue += 0 if self.fixed else random.randrange(-1,2)
-			elif tag.type == "UDINT":
-				tag.lastvalue = tag.lastvalue if tag.lastvalue != None else random.randrange(0,2000)
-				tag.lastvalue += 0 if self.fixed else random.randrange(0,21)
-			elif tag.type == "UINT":
-				tag.lastvalue = tag.lastvalue if tag.lastvalue != None else random.randrange(0,200)
-				tag.lastvalue += 0 if self.fixed else random.randrange(0,3)
-			elif tag.type == "BOOL":
-				tag.lastvalue = tag.lastvalue if tag.lastvalue != None else random.randrange(0,2)
-				tag.lastvalue = tag.lastvalue if (self.fixed or random.randrange(0,10000) < 9998) else not tag.lastvalue
-			
-			return tag.lastvalue
+    def computeValue(self, tag):
+        if tag.type in ["REAL", "LREAL"]:
+            tag.lastvalue = tag.lastvalue if tag.lastvalue != None else (
+                -1000.0 + random.random() * 2000.0)
+            tag.lastvalue += 0 if self.fixed else -10 + random.random() * 20
+        elif tag.type == "DINT":
+            tag.lastvalue = tag.lastvalue if tag.lastvalue != None else random.randrange(
+                -1000, 1000)
+            tag.lastvalue += 0 if self.fixed else random.randrange(-10, 11)
+        elif tag.type == "INT":
+            tag.lastvalue = tag.lastvalue if tag.lastvalue != None else random.randrange(
+                -100, 100)
+            tag.lastvalue += 0 if self.fixed else random.randrange(-1, 2)
+        elif tag.type == "UDINT":
+            tag.lastvalue = tag.lastvalue if tag.lastvalue != None else random.randrange(
+                0, 2000)
+            tag.lastvalue += 0 if self.fixed else random.randrange(0, 21)
+        elif tag.type == "UINT":
+            tag.lastvalue = tag.lastvalue if tag.lastvalue != None else random.randrange(
+                0, 200)
+            tag.lastvalue += 0 if self.fixed else random.randrange(0, 3)
+        elif tag.type == "BOOL":
+            tag.lastvalue = tag.lastvalue if tag.lastvalue != None else random.randrange(
+                0, 2)
+            tag.lastvalue = tag.lastvalue if (
+                self.fixed or random.randrange(0, 10000) < 9998) else not tag.lastvalue
 
-		def computeValues(self, exchange):
-			values = []
-			lasttag = None
-			for tag in exchange.tags:
-				v = self.computeValue(tag)
-				v = v if tag.type != "BOOL" else v << int(tag.offsetbit)
-				if lasttag != None and lasttag.offsetbyte == tag.offsetbyte:
-					values[len(values)-1] = values[len(values)-1] | v
-				else:
-					values.append(v)
-					lasttag = tag
-					
-			return values
+        return tag.lastvalue
 
-		def produce(self, producer, exchange):
-			if self.isRunning():
-				self.count += 1
-				#record last time stamp for dumping purposes by using lasttimestamp field of egd exchange
-				tm = time.localtime()
-				exchange.lasttimestamp = "" + str(self.count) + " at " + str(tm.tm_hour).zfill(2) + ":" + str(tm.tm_min).zfill(2) + ":" + str(tm.tm_sec).zfill(2)
-				hdr = egdmodel.EGDHeader.newHeaderFor(producer.producerid, exchange.exchangenumber, self.count).header
-				payload = struct.pack(exchange.codec, *self.computeValues(exchange))
-				self.sock.sendto(hdr+payload, (self.address, egdmodel.UDP_DATA_PORT))
-				threading.Timer(float(exchange.period) / 1000.0, self.produce, [producer, exchange]).start()
+    def computeValues(self, exchange):
+        values = []
+        lasttag = None
+        for tag in exchange.tags:
+            v = self.computeValue(tag)
+            v = v if tag.type != "BOOL" else v << int(tag.offsetbit)
+            if lasttag != None and lasttag.offsetbyte == tag.offsetbyte:
+                values[len(values)-1] = values[len(values)-1] | v
+            else:
+                values.append(v)
+                lasttag = tag
 
-		def isRunning(self):
-			with self.socklock:
-				return self.sock != None
+        return values
 
-		def stop(self):
-			with self.socklock:
-				if self.sock != None: 
-					self.sock.close()
-					self.sock = None
+    def produce(self, producer, exchange):
+        if self.isRunning():
+            self.count += 1
+            # record last time stamp for dumping purposes by using lasttimestamp field of egd exchange
+            tm = time.localtime()
+            exchange.lasttimestamp = "" + str(self.count) + " at " + str(tm.tm_hour).zfill(
+                2) + ":" + str(tm.tm_min).zfill(2) + ":" + str(tm.tm_sec).zfill(2)
+            hdr = egdmodel.EGDHeader.newHeaderFor(
+                producer.producerid, exchange.exchangenumber, self.count).header
+            payload = struct.pack(
+                exchange.codec, *self.computeValues(exchange))
+            self.sock.sendto(
+                hdr+payload, (self.address, egdmodel.UDP_DATA_PORT))
+            threading.Timer(float(exchange.period) / 1000.0,
+                            self.produce, [producer, exchange]).start()
 
-		def start(self, producerid = None):
-			with self.socklock:
-				self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-				if self.address.endswith(".255"):
-					self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    def isRunning(self):
+        with self.socklock:
+            return self.sock != None
 
-			for producerid in self.egd.producers:
-				producer = self.egd.producers[producerid]
-				for exchangenumber in producer.exchanges:
-					exchange = producer.exchanges[exchangenumber]
-					if self.fixed: self.computeValues(exchange)
-					threading.Timer(float(exchange.period) / 1000.0, self.produce, [producer, exchange]).start()
+    def stop(self):
+        with self.socklock:
+            if self.sock != None:
+                self.sock.close()
+                self.sock = None
 
+    def start(self, producerid=None):
+        with self.socklock:
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            if self.address.endswith(".255"):
+                self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+        for producerid in self.egd.producers:
+            producer = self.egd.producers[producerid]
+            for exchangenumber in producer.exchanges:
+                exchange = producer.exchanges[exchangenumber]
+                if self.fixed:
+                    self.computeValues(exchange)
+                threading.Timer(float(exchange.period) / 1000.0,
+                                self.produce, [producer, exchange]).start()
--- egdsimulator_http.py
+++ egdsimulator_http.py
@@ -1,26 +1,30 @@
-import cgi, sys, getopt
+import cgi
+import sys
+import getopt
 try:
-	from BaseHTTPServer import BaseHTTPRequestHandler
-	import SocketServer
+    from BaseHTTPServer import BaseHTTPRequestHandler
+    import SocketServer
 except:
-	from http.server import BaseHTTPRequestHandler
-	import socketserver as SocketServer
+    from http.server import BaseHTTPRequestHandler
+    import socketserver as SocketServer
 
 import egdmodel
-import MKVIe, Custom
+import MKVIe
+import Custom
 import egdsimulator
+
 
 class MyHandler(BaseHTTPRequestHandler):
 
-	def buildTag(self, tag):
-		return"""<tr class="tag"><td>{}</td><td>{}</td><td>{}</td></tr>""".format(
-				tag.offsetbyte+"."+tag.offsetbit, 
-				tag.type, 
-				tag.lastvalue if tag.lastvalue != None else "")
+    def buildTag(self, tag):
+        return """<tr class="tag"><td>{}</td><td>{}</td><td>{}</td></tr>""".format(
+            tag.offsetbyte+"."+tag.offsetbit,
+            tag.type,
+            tag.lastvalue if tag.lastvalue != None else "")
 
-	def buildExchange(self, exchange):
-		print(exchange.dump())
-		return"""
+    def buildExchange(self, exchange):
+        print(exchange.dump())
+        return """
 			<tr class="exchange">
 				<td>Exchange {}</td>
 				<td>Period {} (ms)</td>
@@ -30,272 +34,291 @@ class MyHandler(BaseHTTPRequestHandler):
 			</tr>
 			{}
 		""".format(
-					exchange.exchangenumber, exchange.period, 
-					exchange.lasttimestamp if exchange.lasttimestamp != None else "", 
-					len(exchange.tags),
-					"".join(list(map(lambda t: self.buildTag(t), exchange.sortedTags()))))
+            exchange.exchangenumber, exchange.period,
+            exchange.lasttimestamp if exchange.lasttimestamp != None else "",
+            len(exchange.tags),
+            "".join(list(map(lambda t: self.buildTag(t), exchange.sortedTags()))))
 
-	def buildProducer(self, producer):
-		return"""
+    def buildProducer(self, producer):
+        return """
 			<tr class="producer"><td>Producer {}</td></tr>
 			{}
 		""".format(
-					producer.producerid, 
-					"".join(map(lambda e: self.buildExchange(producer.exchanges[e]), producer.exchanges.keys())))
+            producer.producerid,
+            "".join(map(lambda e: self.buildExchange(producer.exchanges[e]), producer.exchanges.keys())))
 
-	def buildTable(self, configuration):
-		return"""
+    def buildTable(self, configuration):
+        return """
 			<table><tbody>{}</tbody></table>
 		""".format(
-					"".join(list(
-							map(lambda p: self.buildProducer(configuration.producers[p]), configuration.producers.keys()))
-						)
-			)
+            "".join(list(
+                map(lambda p: self.buildProducer(configuration.producers[p]), configuration.producers.keys()))
+            )
+        )
 
-	def buildPage(self, simulator, state):
-		page = (open("resources/index.html")).read()
-		patches = { 
-			"error" : state["error"],
-			"errorvisible" : "" if state["error"] != None else "invisible",
-  		"delimiter" : state["delimiter"],
-  		"skiplines" : state["skiplines"],
-  		"address" : simulator.address,
-  		"fixed" : "checked" if simulator.fixed else "",
-			"displaystart" : "inactive" if simulator.isRunning() else "active",
-			"displaystop" : "active" if simulator.isRunning() else "inactive",
-			"egd" : self.buildTable(simulator.egd) if simulator.egd != None else ""
-		}
-		page = page.format(**patches)
-		state["error"] = None
-		return page
-	
-	def buildDocumentationPage(self):
-		page = (open("resources/documentation.html")).read()
-		patches = { 
-			"license" : open("LICENSE").read().replace("\n", "<br/>"),
-			"authors" : open("AUTHORS").read().replace("\n", "<br/>"),
-			"manual" : open("README.md").read().replace("\n", "<br/>")
-		}
-		page = page.format(**patches)
-		return page
-	
-	def returnPage(self, page):
-		self.send_response(200)
-		self.send_header('Content-type', 'text/html')
-		self.end_headers()
-		if sys.version_info < (3,):
-			self.wfile.write(page)
-		else:
-			self.wfile.write(bytes(page, "UTF-8"))
+    def buildPage(self, simulator, state):
+        page = (open("resources/index.html")).read()
+        patches = {
+            "error": state["error"],
+            "errorvisible": "" if state["error"] != None else "invisible",
+            "delimiter": state["delimiter"],
+            "skiplines": state["skiplines"],
+            "address": simulator.address,
+            "fixed": "checked" if simulator.fixed else "",
+            "displaystart": "inactive" if simulator.isRunning() else "active",
+            "displaystop": "active" if simulator.isRunning() else "inactive",
+            "egd": self.buildTable(simulator.egd) if simulator.egd != None else ""
+        }
+        page = page.format(**patches)
+        state["error"] = None
+        return page
 
-	def returnResource(self, resource, mediatype):
-		self.send_response(200)
-		self.send_header('Content-type', (mediatype if mediatype != None else ""))
-		self.end_headers()
-		self.wfile.write(open(resource, "rb").read())
+    def buildDocumentationPage(self):
+        page = (open("resources/documentation.html")).read()
+        patches = {
+            "license": open("LICENSE").read().replace("\n", "<br/>"),
+            "authors": open("AUTHORS").read().replace("\n", "<br/>"),
+            "manual": open("README.md").read().replace("\n", "<br/>")
+        }
+        page = page.format(**patches)
+        return page
 
-	def redirect(self, location):
-		self.send_response(301)
-		self.send_header('Location',location)
-		self.end_headers()
+    def returnPage(self, page):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        if sys.version_info < (3,):
+            self.wfile.write(page)
+        else:
+            self.wfile.write(bytes(page, "UTF-8"))
 
-	def do_GET(self):
+    def returnResource(self, resource, mediatype):
+        self.send_response(200)
+        self.send_header(
+            'Content-type', (mediatype if mediatype != None else ""))
+        self.end_headers()
+        self.wfile.write(open(resource, "rb").read())
 
-		global simulator
-		global state
+    def redirect(self, location):
+        self.send_response(301)
+        self.send_header('Location', location)
+        self.end_headers()
 
-		if self.path == "/":
-			self.returnPage(self.buildPage(simulator, state))
-			return
-		if self.path == "/logo":
-			self.returnResource("resources/logonubisware.png", "image/png")
-			return
-		if self.path == "/favicon.ico":
-			self.returnResource("resources/favicon.ico", "image/ico")
-			return
-		if self.path == "/documentation":
-			self.returnPage(self.buildDocumentationPage())
-			return
-		else:
-			self.redirect("/")
-			return
+    def do_GET(self):
 
-	def parseMultipartPy2(self):
-		ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
-		if not ctype == "multipart/form-data":
-			self.redirect("/")
-			return
+        global simulator
+        global state
 
-		query = cgi.parse_multipart(self.rfile, pdict)
+        if self.path == "/":
+            self.returnPage(self.buildPage(simulator, state))
+            return
+        if self.path == "/logo":
+            self.returnResource("resources/logonubisware.png", "image/png")
+            return
+        if self.path == "/favicon.ico":
+            self.returnResource("resources/favicon.ico", "image/ico")
+            return
+        if self.path == "/documentation":
+            self.returnPage(self.buildDocumentationPage())
+            return
+        else:
+            self.redirect("/")
+            return
 
-		mkvie = query.get("mkvie")[0] if query.get("mkvie")[0].decode("utf-8-sig") != "" else None
-		custom = query.get("custom")[0] if query.get("custom")[0].decode("utf-8-sig") != "" else None
+    def parseMultipartPy2(self):
+        ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+        if not ctype == "multipart/form-data":
+            self.redirect("/")
+            return
 
-		state["delimiter"] = query.get("delimiter")[0]
-		state["skiplines"] = int(query.get("skiplines")[0])
-		
-		return (mkvie, custom)
-			
-	def parseMultipartPy3(self):
-		global state
-		ctype, pdict = cgi.parse_header(self.headers.get('content-type'))
-						
-		if not ctype == "multipart/form-data":
-			self.redirect("/")
-			return
+        query = cgi.parse_multipart(self.rfile, pdict)
 
-		pdict["boundary"] = bytes(pdict["boundary"], "utf-8")
-			
-		query = cgi.parse_multipart(self.rfile, pdict)
-		
-		mkvie = query.get("mkvie")[0] if query.get("mkvie")[0].decode("utf-8-sig") != "" else None
-		custom = query.get("custom")[0] if query.get("custom")[0].decode("utf-8-sig") != "" else None
+        mkvie = query.get("mkvie")[0] if query.get("mkvie")[
+            0].decode("utf-8-sig") != "" else None
+        custom = query.get("custom")[0] if query.get("custom")[
+            0].decode("utf-8-sig") != "" else None
 
-		state["delimiter"] = (query.get("delimiter")[0]).decode("utf-8")
-		state["skiplines"] = int(query.get("skiplines")[0])
-		
-		return (mkvie, custom)
+        state["delimiter"] = query.get("delimiter")[0]
+        state["skiplines"] = int(query.get("skiplines")[0])
 
-	def do_POST(self):
+        return (mkvie, custom)
 
-		global simulator
-		global state
+    def parseMultipartPy3(self):
+        global state
+        ctype, pdict = cgi.parse_header(self.headers.get('content-type'))
 
-		if self.path == "/start":
-			ctype, pdict = cgi.parse_header(self.headers.get('content-type'))
-			
-			try:
-				pdict["boundary"] = bytes(pdict["boundary"], "utf-8") #fix 2 to 3 bug
-			except:
-				pdict["boundary"] = bytes(pdict["boundary"])
-				
-			if not ctype == "multipart/form-data":
-				self.returnPage(self.buildPage())
-				return
-			query = cgi.parse_multipart(self.rfile, pdict)
+        if not ctype == "multipart/form-data":
+            self.redirect("/")
+            return
 
-			simulator.fixed = query.get("fixed") != None and query.get("fixed")[0] != ""
+        pdict["boundary"] = bytes(pdict["boundary"], "utf-8")
 
-			if query.get("address")[0] != None and query.get("address")[0] != "":
-				simulator.address = (query.get("address")[0]).decode("utf-8")
+        query = cgi.parse_multipart(self.rfile, pdict)
 
-			if not simulator.isRunning():
-				simulator.start()
-			self.redirect("/")
+        mkvie = query.get("mkvie")[0] if query.get("mkvie")[
+            0].decode("utf-8-sig") != "" else None
+        custom = query.get("custom")[0] if query.get("custom")[
+            0].decode("utf-8-sig") != "" else None
 
-		if self.path == "/stop":
-			if simulator.isRunning():
-				simulator.stop()
-			self.redirect("/")
+        state["delimiter"] = (query.get("delimiter")[0]).decode("utf-8")
+        state["skiplines"] = int(query.get("skiplines")[0])
 
-		if self.path == "/configure":
+        return (mkvie, custom)
 
-			mkvie = None
-			custom = None
-			if sys.version_info < (3,):
-				mkvie, custom = self.parseMultipartPy2()
-			else:
-				mkvie, custom = self.parseMultipartPy3()
-			
-			wasrunning = simulator.isRunning()
-			if wasrunning: simulator.stop()
+    def do_POST(self):
 
-			if mkvie != None and mkvie != "" and mkvie != b"":
-				print("Parsing MKVIE")
-				newegd = egdmodel.EGDConfiguration()
-				try:
-					MKVIe.Parser(newegd).parse(mkvie, state["delimiter"], state["skiplines"])
-				except Exception as e:
-					state["error"] = "Unable to parse MKVIe CSV: " + str(e) + str(sys.exc_info()[2].tb_lineno)
-					self.redirect("/")
+        global simulator
+        global state
 
-				simulator.egd = newegd
-				print(simulator.egd.dump())
+        if self.path == "/start":
+            ctype, pdict = cgi.parse_header(self.headers.get('content-type'))
 
-			elif custom != None and custom != "" and custom != b"":
-				print("Parsing Custom")
-				newegd = egdmodel.EGDConfiguration()
-				try:
-					Custom.Parser(newegd).parse(custom, state["delimiter"], state["skiplines"])
-				except Exception as e:
-					state["error"] = "Unable to parse Custom CSV: " + str(e) + str(sys.exc_info()[2].tb_lineno)
-					self.redirect("/")
+            try:
+                pdict["boundary"] = bytes(
+                    pdict["boundary"], "utf-8")  # fix 2 to 3 bug
+            except:
+                pdict["boundary"] = bytes(pdict["boundary"])
 
-				simulator.egd = newegd
-				print(simulator.egd.dump())
+            if not ctype == "multipart/form-data":
+                self.returnPage(self.buildPage())
+                return
+            query = cgi.parse_multipart(self.rfile, pdict)
 
-			if wasrunning: 
-				simulator.start()
+            simulator.fixed = query.get(
+                "fixed") != None and query.get("fixed")[0] != ""
 
-			self.redirect("/")
+            if query.get("address")[0] != None and query.get("address")[0] != "":
+                simulator.address = (query.get("address")[0]).decode("utf-8")
 
-			return
+            if not simulator.isRunning():
+                simulator.start()
+            self.redirect("/")
 
-		else:
-			self.redirect("/")
-			return
+        if self.path == "/stop":
+            if simulator.isRunning():
+                simulator.stop()
+            self.redirect("/")
+
+        if self.path == "/configure":
+
+            mkvie = None
+            custom = None
+            if sys.version_info < (3,):
+                mkvie, custom = self.parseMultipartPy2()
+            else:
+                mkvie, custom = self.parseMultipartPy3()
+
+            wasrunning = simulator.isRunning()
+            if wasrunning:
+                simulator.stop()
+
+            if mkvie != None and mkvie != "" and mkvie != b"":
+                print("Parsing MKVIE")
+                newegd = egdmodel.EGDConfiguration()
+                try:
+                    MKVIe.Parser(newegd).parse(
+                        mkvie, state["delimiter"], state["skiplines"])
+                except Exception as e:
+                    state["error"] = "Unable to parse MKVIe CSV: " + \
+                        str(e) + str(sys.exc_info()[2].tb_lineno)
+                    self.redirect("/")
+
+                simulator.egd = newegd
+                print(simulator.egd.dump())
+
+            elif custom != None and custom != "" and custom != b"":
+                print("Parsing Custom")
+                newegd = egdmodel.EGDConfiguration()
+                try:
+                    Custom.Parser(newegd).parse(
+                        custom, state["delimiter"], state["skiplines"])
+                except Exception as e:
+                    state["error"] = "Unable to parse Custom CSV: " + \
+                        str(e) + str(sys.exc_info()[2].tb_lineno)
+                    self.redirect("/")
+
+                simulator.egd = newegd
+                print(simulator.egd.dump())
+
+            if wasrunning:
+                simulator.start()
+
+            self.redirect("/")
+
+            return
+
+        else:
+            self.redirect("/")
+            return
+
 
 simulator = None
 state = {
-	"skiplines" : 2,
-	"delimiter" : ",",
-	"error" : None
+    "skiplines": 2,
+    "delimiter": ",",
+    "error": None
 }
 
+
 def dumpUsage():
-	print("python " + sys.argv[0] + ' [-p <port>] [-a <initial send address>]')
+    print("python " + sys.argv[0] + ' [-p <port>] [-a <initial send address>]')
+
 
 def boot(argv):
-	global simulator
-	global state
-	HTTP_PORT = 8000
-	INIT_ADDRESS = "127.0.0.255"
-	#INIT_CSV = None
-	try:
-		opts, args = getopt.getopt(argv[1:],"hp:a:")
-		for opt, arg in opts:
-			if opt == "-h":
-				dumpUsage()
-				sys.exit(0)
-			elif opt == "-p":
-				HTTP_PORT = int(arg)
-			elif opt == "-a":
-				INIT_ADDRESS = int(arg)
+    global simulator
+    global state
+    HTTP_PORT = 8000
+    INIT_ADDRESS = "127.0.0.255"
+    # INIT_CSV = None
+    try:
+        opts, args = getopt.getopt(argv[1:], "hp:a:")
+        for opt, arg in opts:
+            if opt == "-h":
+                dumpUsage()
+                sys.exit(0)
+            elif opt == "-p":
+                HTTP_PORT = int(arg)
+            elif opt == "-a":
+                INIT_ADDRESS = int(arg)
 
-	except getopt.GetoptError:
-		dumpUsage()
-		sys.exit(2)
+    except getopt.GetoptError:
+        dumpUsage()
+        sys.exit(2)
 
-	#Initialize with empty configuration and default values for fixed and address
-	simulator = egdsimulator.EGDSimulator(egdmodel.EGDConfiguration(), INIT_ADDRESS, False)
+    # Initialize with empty configuration and default values for fixed and address
+    simulator = egdsimulator.EGDSimulator(
+        egdmodel.EGDConfiguration(), INIT_ADDRESS, False)
 
-	httpd = SocketServer.TCPServer(("", HTTP_PORT), MyHandler, bind_and_activate=False)
+    httpd = SocketServer.TCPServer(
+        ("", HTTP_PORT), MyHandler, bind_and_activate=False)
 
-	httpd.allow_reuse_address = True
-	httpd.daemon_threads = True
-	try:
-		print("\nCreating HTTP server...")
-		httpd.server_bind()
-		httpd.server_activate()
-		print(sys.argv[0] + " HTTP server is listening on port " + str(HTTP_PORT) + "...")
-		httpd.serve_forever()
-	except KeyboardInterrupt as e:
-		print("Interrupted by user...")
-	except Exception as e:
-		print(e)
-		print(traceback.format_exc())
+    httpd.allow_reuse_address = True
+    httpd.daemon_threads = True
+    try:
+        print("\nCreating HTTP server...")
+        httpd.server_bind()
+        httpd.server_activate()
+        print(sys.argv[0] + " HTTP server is listening on port " +
+              str(HTTP_PORT) + "...")
+        httpd.serve_forever()
+    except KeyboardInterrupt as e:
+        print("Interrupted by user...")
+    except Exception as e:
+        print(e)
+        print(traceback.format_exc())
 
-	try:
-		print("Stopping simulator...")
-		simulator.stop()
-		print("Stopping HTTP server...")
-		httpd.shutdown()
-		httpd.server_close()
-	except Exception as e:
-		print(e)
-		print(traceback.format_exc())
+    try:
+        print("Stopping simulator...")
+        simulator.stop()
+        print("Stopping HTTP server...")
+        httpd.shutdown()
+        httpd.server_close()
+    except Exception as e:
+        print(e)
+        print(traceback.format_exc())
 
-	sys.exit(1)
+    sys.exit(1)
+
 
 if __name__ == "__main__":
-	boot(sys.argv)
+    boot(sys.argv)
